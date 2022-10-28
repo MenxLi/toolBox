a3f0229835deb6a935ec7cf205b0a49f2406d428
limengxun <limengxun45@outlook.com>
2022-10-28 22:07:15 +0800

    feat(tbx_countline): multi-directory summary by extension

--- monsoonToolBox/cmdCall/devUtils.py
+++ monsoonToolBox/cmdCall/devUtils.py
@@ -9,7 +9,7 @@ def countLine() -> None:
     i.e. countLine . -s .py .json -i .git build\
     "
     parser = argparse.ArgumentParser(description=_description)
-    parser.add_argument("path", type = str)
+    parser.add_argument("path", type = str, nargs="+")
     parser.add_argument("-s", "--suffix", nargs="+", default=[".txt", ".py", ".js", ".c", ".h", ".html", ".json"])
     parser.add_argument("-i", "--ignore", nargs="+", default=["dist", "__pycache__", ".git", "build"])
     args = parser.parse_args()
@@ -29,7 +29,9 @@ def countLine() -> None:
                 file_valid += _getFile_recursive(f_path, suffix)
         return file_valid
     
-    valid_files = _getFile_recursive(args.path, args.suffix)
+    valid_files = []
+    for p in args.path:
+        valid_files += _getFile_recursive(p, args.suffix)
     outcome = dict()
     for k in args.suffix:
         outcome[k] = {
@@ -37,15 +39,25 @@ def countLine() -> None:
             "count":0
         }
     total_count = 0
+    lines_by_suffix = {}
     for f in valid_files:
         suffix_ = "."+f.split(".")[-1]
+        lines_by_suffix.setdefault(suffix_, 0)
         with open(f, "r") as fp:
             count_ = len(fp.readlines())
             total_count += count_
+            lines_by_suffix[suffix_] += count_
         outcome[suffix_]["files"].append(f)
         outcome[suffix_]["count"] += count_
     pprint.pprint(outcome)
+    print()
     print("Total: ", total_count)
+
+    print('==========================')
+    print("Number of lines by file extension: ")
+    for s in lines_by_suffix:
+        print(f" >> {s}: {lines_by_suffix[s]}")
+    print('==========================')
 
 
 def crun() -> None:
