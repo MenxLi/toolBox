2634710bfbf3e2bafa0397226bf7df04fd4aafe2
monsoon-pro <mengxunli@whu.edu.cn>
2022-12-04 10:28:14 +0800

    minor

--- monsoonToolBox/arraytools/__init__.py
+++ monsoonToolBox/arraytools/__init__.py
@@ -1,7 +1,10 @@
-from .draw2d import *
+try:
+	from .draw2d import *
 #  from .draw3d import *
-from .arrayBase import *
-from .maskNd import *
-from .mask2d import *
-from .mask3d import *
-from .img2d import *
+	from .arrayBase import *
+	from .maskNd import *
+	from .mask2d import *
+	from .mask3d import *
+	from .img2d import *
+except ImportError:
+	pass
