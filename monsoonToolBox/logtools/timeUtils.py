b442034ce065517b32cf67a702584361efeef4ab
monsoon-pro <mengxunli@whu.edu.cn>
2023-01-21 17:27:08 +0800

    timed leading

--- monsoonToolBox/logtools/timeUtils.py
+++ monsoonToolBox/logtools/timeUtils.py
@@ -6,9 +6,10 @@ def getDateTimeNumStr():
     return str(datetime.datetime.now())[:-7]
 
 class Timer(object):
-    def __init__(self, flag = "this operation") -> None:
+    def __init__(self, flag = "this operation", leading = "======> ") -> None:
         super().__init__()
         self.flag = flag
+        self.leading = leading
 
     def __enter__(self):
         self.start_time = time.time()
@@ -16,6 +17,6 @@ class Timer(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end_time = time.time()
         t = self.end_time - self.start_time
-        time_str = "======> Time for {flag} is: {time}s".format(\
-            flag = self.flag, time = t)
+        time_str = "{leading}Time for {flag} is: {time}s".format(\
+            leading = self.leading, flag = self.flag, time = t)
         print(time_str)
